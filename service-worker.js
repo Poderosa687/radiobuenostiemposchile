f782809b1dc9acaad117dc404af0652580ab00e6
Poderosa687 <133808541+Poderosa687@users.noreply.github.com>
2025-08-15 09:37:54 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+
+const CACHE_NAME = 'radio-buenos-tiempos-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://i.postimg.cc/9ft0sz5B/logo-1200x1200.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
